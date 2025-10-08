7ee3f3196cf815728a9da2f601851c0e281b723b
Passang Passang <08230164.sce@rub.edu.bt>
2025-10-08 12:26:02 +0530

    initail changes

--- myFirstProject/MyApp/models.py
+++ myFirstProject/MyApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+   name = models.CharField(max_length=50)
+   studentID = models.CharField(max_length=8)
+   age = models.IntegerField()
--- myFirstProject/MyApp/views.py
+++ myFirstProject/MyApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Student
+
 
 # Create your views here.
-def index (request):
-    return render(request,'index.html')
+def index(request):
+    students = Student.objects.all()
+    context = {'students': students}
+    return render(request, 'index.html', context)
